e57f99f1363bd4d9ad804bd1322120d5675f88fb
m <m@localhost>
2025-03-10 22:01:01 +0100

    writer added syslog

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -2,27 +2,37 @@
 #include "stdint.h"
 #include "stdio.h"
 
-#include <sys/stat.h>
+#include "syslog.h"
 
 int main(int argc, char** argv)
 {
-	struct stat stats;
-
-	if (argc != 3) 
+	openlog(NULL, 0, LOG_USER);
+	
+	//first is application, therfore -1 to get user supplied args
+	if ((argc-1) != 2) 
 	{
 		printf("Usage: write <path> <print string>\n");
+		syslog(LOG_ERR, "not enough arguments got %d, expected 2", argc);
 		return 1;
 	}
 
 	const char* filePath = argv[1];
 	const char* printString = argv[2];
 
+	syslog(LOG_DEBUG, "Writing message %s to file %s", filePath, printString);
+	closelog();
+
 	FILE* mFile = fopen(filePath, "wt"); //no append
-	if (mFile)
+	if (!mFile)
 	{
-		fprintf(mFile, "%s\n", printString);
-		fclose(mFile);
+		syslog(LOG_ERR, "could not create file %s", filePath);
+		closelog();
+		return 1;
 	}
 
-	return 1;
+	fprintf(mFile, "%s\n", printString);
+	fclose(mFile);
+	syslog(LOG_USER, "string write sucess");
+	closelog();
+	return 0;
 }
